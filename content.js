6da0060ccfb4650eafb0dfb4cd152b96c37ea3f7
Colin Zhou <colin.cosun@gmail.com>
2020-12-22 12:17:33 -0800

    com. tuesday

--- content.js
+++ content.js
@@ -1,46 +1,37 @@
 
 
 function messageBackground() {
-  window.onload = function() {
-    if (window.location.href == 'https://www.youtube.com/') {
-      history.pushState({}, '', './statehome');
+  if (window.location.href == 'https://www.youtube.com/') {
+    console.log('testingwerqwe')
+    history.pushState({id: 2}, '', './statehome');
+    console.log(history.state)
+  }
 
-    } else if (window.location.href.includes('/results')) {
-      // chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-      //   console.log('changedstate');
-      // });
-      console.log('bts')
-      history.replaceState({}, '', './stateresult');
-
-    }
-
+  if (window.location.href.includes('/results')) {
+    // chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    //   console.log('changedstate');
+    // });
+    //history.go(0);
+    console.log('bts')
+    history.replaceState({id: 1}, '', './stateresult');
   }
   window.addEventListener('yt-navigate-start', function() {
-
-    // chrome.runtime.connect().onDisconnect.addListener(function() {
-    //   chrome.runtime.sendMessage({message: 'reconnect'});
-    //   console.log('disconnected');
-    //   // clean up when content script gets disconnected
-    // })
-    // if (window.readyState === "complete") {
-      
-    // }
-
-    if (window.location.href == 'https://www.youtube.com/') { //don't inject
-    history.pushState({}, '', './statehome')
-    chrome.runtime.sendMessage({message: 'home'});
-    var port = chrome.runtime.connect({name: "colin"});
-    port.postMessage({joke: "hey wassup"});
-    console.log('testing');
     
-  } else if (window.location.href.includes('/results')) { //inject css
-      //history.pushState({id: 'results'}, '', 'searchedVideos');       
-      chrome.runtime.sendMessage({message: 'results'});
-      console.log('working 2');
+  //   if (window.location.href == 'https://www.youtube.com/') { //don't inject
+  //   history.pushState({}, '', './statehome')
+  //   chrome.runtime.sendMessage({message: 'home'});
+  //   var port = chrome.runtime.connect({name: "colin"});
+  //   port.postMessage({joke: "hey wassup"});
+  //   console.log('testing');
+    
+  // } else if (window.location.href.includes('/results')) { //inject css
+  //     //history.pushState({id: 'results'}, '', 'searchedVideos');       
+  //     chrome.runtime.sendMessage({message: 'results'});
+  //     console.log('working 2');
 
-    } else if (window.location.href.includes('/watch')) {
-      chrome.runtime.sendMessage({message: 'watch'});
-    }
+  //   } else if (window.location.href.includes('/watch')) {
+  //     chrome.runtime.sendMessage({message: 'watch'});
+  //   }
     
     // if (chrome.runtime.lastError) {
     //   setTimeout(messageBackground, 1000);
