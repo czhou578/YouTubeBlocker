54ebf27d3d6a8e69a4d738868d02d858fa5a151a
Colin Zhou <colin.cosun@gmail.com>
2020-12-25 18:26:50 -0800

    final commit friday

--- background.js
+++ background.js
@@ -1,38 +1,30 @@
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  chrome.tabs.executeScript(null,{file:"content.js"});
-
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"});
-  // });
-  
+  chrome.tabs.executeScript(null,{file:"content.js"});  
   chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
   
     if (response.message === "results") {
-      //history.pushState({id: 1}, '', "/searched");
       chrome.tabs.removeCSS(null, {file: "home.css"});
       chrome.tabs.removeCSS(null, {file: "videoRec.css"});
       chrome.tabs.insertCSS(null,{file:"results.css"});
-      console.log('received results!');
+      //console.log('received results!');
     }
   
     if (response.message === "watch") {
-      //history.pushState({id: 2}, '', "/watched");
-      console.log('received watch!')
+      //console.log('received watch!')
       chrome.tabs.insertCSS(null, {file: "videoRec.css"});
     }
     
     if (response.message === "home") {
-      //history.pushState({id: 0}, '', "https://wwww.youtube.com/homepage");
       chrome.tabs.insertCSS(null, {file: "home.css", runAt:"document_end"});
       chrome.tabs.removeCSS(null,{file:"results.css"});
       chrome.tabs.removeCSS(null, {file: "videoRec.css"});
-      console.log('received home!')
+      //console.log('received home!')
   
     }
 
     if (response.message === "user") {
-      console.log('users');
+      //console.log('users');
       chrome.tabs.insertCSS(null, {file: "channelUser.css"});
     }
   
--- content.js
+++ content.js
@@ -1,35 +1,3 @@
-
-console.log('sun rui')
-// function messageBackground() {
-//   if (window.location.href == 'https://www.youtube.com/') {
-//     //history.replaceState({id: 0}, '',);
-//     chrome.runtime.sendMessage({message: 'home'});
-//   }
-  
-//   window.addEventListener('yt-navigate-start', function() {
-//     if (window.location.href.includes('/results')) {
-//       //history.replaceState({id: 1}, '');
-//       chrome.runtime.sendMessage({message: 'results'});
-
-//     } else if ('/watch' === location.pathname) {
-//       //history.replaceState({id: 2}, '');
-//       chrome.runtime.sendMessage({message: 'watch'});
-//     }
-//   })
-// }
-
-
-// window.onload = function() {
-  
-  //   if (window.location.href.includes('/results')) {
-    //     chrome.runtime.sendMessage({message: 'results'});
-    
-    //   } else if (window.location.href === 'https://www.youtube.com/') {
-      //     chrome.runtime.sendMessage({message: 'home'});
-      
-      //   }
-      // }
-      
   function replyBack() {
     if (window.location.href.includes('/results')) {
       chrome.runtime.sendMessage({message: 'results'});
@@ -49,22 +17,6 @@ console.log('sun rui')
   }
   
   replyBack();
-      
-  //setTimeout(replyBack, 2000);  
-  
-  // function idChange() {
-  //   if (history.state && history.state.id === 1) {
-  //     console.log('id1')
-  //   } else if (history.state && history.state.id === 2) {
-  //     console.log('id2');
-  //   } else if (history.state && history.state.id === 0) {
-  //     console.log('id0');
-  //   }
-
-  // }
-
-//messageBackground();
-
 
 
 
