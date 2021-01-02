a2865f1473a5f2dcc86452ab6048462bdb799037
Colin Zhou <colin.cosun@gmail.com>
2021-01-01 21:21:39 -0800

    commit

--- background.js
+++ background.js
@@ -6,32 +6,32 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.assert(port.name == "connection");
     port.onMessage.addListener(function(msg) {
       if (msg.msg === "results") {
-        chrome.tabs.removeCSS(null, {file: "home.css"});
-        chrome.tabs.removeCSS(null, {file: "videoRec.css"});
-        chrome.tabs.insertCSS(null,{file:"results.css", runAt:"document_idle"});
+        chrome.tabs.removeCSS(null, {file: "home.css", runAt:"document_end"});
+        chrome.tabs.removeCSS(null, {file: "videoRec.css", runAt:"document_end"});
+        chrome.tabs.insertCSS(null,{file:"results.css", runAt:"document_end"});
 
       }
 
       if (msg.msg === "watch") {
-        chrome.tabs.removeCSS(null, {file: "videoRec.css"});
-        chrome.tabs.insertCSS(null, {file: "videoRec.css"});
+        chrome.tabs.removeCSS(null, {file: "videoRec.css", runAt:"document_end"});
+        chrome.tabs.insertCSS(null, {file: "videoRec.css", runAt:"document_end"});
 
       }
 
       if (msg.msg === "home") {
-        chrome.tabs.insertCSS(null, {file: "home.css", runAt:"document_idle"});
-        chrome.tabs.removeCSS(null,{file:"results.css"});
-        chrome.tabs.removeCSS(null, {file: "videoRec.css"});  
+        chrome.tabs.insertCSS(null, {file: "home.css", runAt:"document_end"});
+        chrome.tabs.removeCSS(null,{file:"results.css", runAt:"document_end"});
+        chrome.tabs.removeCSS(null, {file: "videoRec.css", runAt:"document_end"});  
 
       }
 
       if (msg.msg === "user") {
-        chrome.tabs.insertCSS(null, {file: "channelUser.css"});
+        chrome.tabs.insertCSS(null, {file: "channelUser.css", runAt:"document_end"});
       }
 
       if (msg.msg === "channel") {
-        chrome.tabs.removeCSS(null, {file: "videoRec.css"});
-        chrome.tabs.insertCSS(null, {file: "channelUser.css"});
+        chrome.tabs.removeCSS(null, {file: "videoRec.css", runAt:"document_end"});
+        chrome.tabs.insertCSS(null, {file: "channelUser.css", runAt:"document_end"});
 
       }
 
@@ -41,7 +41,7 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
 
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
   if (response.message === "outsideIn") {
-    chrome.tabs.insertCSS(null, {file: "videoRec.css"});
+    chrome.tabs.insertCSS(null, {file: "videoRec.css", runAt:"document_end"});
   }
 
 });
